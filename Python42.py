bcde8416554825d011935102bcdee169f546c7dd
178200623 <178200623@qq.com>
2018-05-07 22:49:52 +0800

    20180507

---
+++ Python42.py
@@ -0,0 +1,28 @@
+#-*- coding:UTF-8 -*-
+#笨办法学编程py3---类
+class TheThing(object):
+
+    def __init__(self):
+        self.number = 0
+
+    def some_function(self):
+        print("I got called.")
+
+    def add_me_up(self,more):
+        self.number += more
+        return  self.number
+
+
+a = TheThing()
+b = TheThing()
+
+a.some_function()
+b.some_function()
+
+print(a.add_me_up(20))
+print(a.add_me_up(20))
+print(b.add_me_up(30))
+print(b.add_me_up(30))
+
+print(a.number)
+print(b.number)
---
+++ Python47.py
@@ -0,0 +1,2 @@
+from nose import *
+from python.Py47.Py47 import Room
